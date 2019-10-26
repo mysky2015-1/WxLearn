aad28a60fc8ba5b4ce494f34354cb22921b27d0d
zm <zm@zhaomideMacBook-Pro.local>
2019-10-27 07:47:14 +0800

    react-test07

--- react/app/src/test/demo07.js
+++ react/app/src/test/demo07.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react'
-import {render,unmountComponentAtNode} from 'react-dom'
-
+import ReactDOM from 'react-dom';
 // 请写一个满足以下要求的confirm方法组件：
 // （1）能在任意组件(示例如下)的componentDidMount生命周期中挂载，并返回一个promise；
 // （2）能通过该promise返回的结果判断confirm组件是否成功挂载。
@@ -12,27 +11,55 @@ import {render,unmountComponentAtNode} from 'react-dom'
 //       console.log("否")
 //   }
 // }
-
-import { confirm } from './api'
-let root = document.getElementById('root')
-class Init extends Component {
-  render () {
-    return (
-      <div>
-        loadig...
-      </div>
-    )
+class Confirm extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { status: null };
   }
+  render() {
+		return (
+			<div className="confirm">
+        这是confirm~
+        <p>{this.props.message}</p>
+			</div>
+		);
+	}
+  componentDidMount(){
+    this.setState({ status: 'DidMount' }); 
+    console.log('Confirm挂载成功~',this.props.message)
+  }
+  componentWillUnmount() {
+    this.setState({ status: 'WillUnmount' });
+  }
+}
+const confirm = message=> {
+  return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(ReactDOM.render(
+          <Confirm message={message}/>,
+          document.getElementById('root')
+        ))
+      },3000)
+  })
+}
 
+class App extends Component{
+  render() {
+		return (
+			<div>
+				app
+			</div>
+		);
+	}
   async componentDidMount() {
     let res = await confirm("确定删除吗")
+    console.log('confirm组件是否挂载成功返回值',res)
     if(res) {
-        console.log("是")
+        console.log('confirm组件是否挂载成功: 是')
     } else {
-        console.log("否")
+        console.log('confirm组件是否挂载成功: 否')
     }
   }
 }
 
-
-render(<Init/>,root)
+export default App
